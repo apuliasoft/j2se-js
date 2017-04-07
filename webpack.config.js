a6ebab649bfa324a764ac207803855da2a167395
CarlottaDim <carlottaunspekable@gmail.com>
2017-04-07 15:55:36 +0200

    chore(setup): remove useless config
    removed useless config from webpack and changed name and entrypoint
    
    #3

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
 module.exports = {
  entry: './src/index.ts',
  output: {
-   filename: './dist/util.js',
+   filename: './dist/core.js',
    path: __dirname
  },
  module: {
    rules: [
      {
-       test: /\.tsx?$/,
        loader: 'ts-loader',
        exclude: /node_modules/,
      },
    ]
  },
  resolve: {
-   extensions: [".tsx", ".ts", ".js"]
+   extensions: [".ts"]
  },
 };
