4f93fd3f4ec28c2b3820a28d8fb54bb3b722be0c
Francesco Dammacco <Fra@MacBook-Air-di-Francesco.local>
2017-07-14 11:54:51 +0200

    feat(java.lang): first implementation of java.lang.Object

---
+++ packages/java.lang/test/jobject.spec.ts
@@ -0,0 +1,86 @@
+import {JObject} from '../src/jobject';
+import {expect} from 'chai';
+import 'mocha';
+
+describe('JObject', () => {
+    it('getClass should return the Class object that represents its runtime class', () => {
+        const obj = new JObject();
+        obj.getClass();
+    });
+
+    it('equals should return false if it is called with another JObject as argument', () => {
+        const obj = new JObject();
+        const obj2 = new JObject();
+        expect(obj.equals(obj2)).to.be.false;
+    });
+
+    it('equals should return false if it is called with null argument', () => {
+        const obj = new JObject();
+        expect(obj.equals(null)).to.be.false;
+    });
+
+    it('equals should be reflexive', () => {
+        const obj = new JObject();
+        expect(obj.equals(obj)).to.be.true;
+    });
+
+    it('equals should be symmetric', () => {
+        const obj = new JObject();
+        const obj2 = new JObject();
+        expect(obj.equals(obj2)).to.be.false;
+        expect(obj2.equals(obj)).to.be.false;
+    });
+
+    it('equals should be transitive', () => {
+        const obj = new JObject();
+        const obj2 = new JObject();
+        const obj3 = new JObject();
+        expect(obj.equals(obj2)).to.be.false;
+        expect(obj2.equals(obj3)).to.be.false;
+        expect(obj.equals(obj3)).to.be.false;
+    });
+
+    it('equals should be consistent', () => {
+        const obj = new JObject();
+        const obj2 = new JObject();
+
+        expect(obj.equals(obj)).to.be.true;
+        expect(obj.equals(obj)).to.be.true;
+        expect(obj.equals(obj2)).to.be.false;
+        expect(obj.equals(obj2)).to.be.false;
+    });
+
+    it('hashCode should consistently return the same integer if it is invoked on the same object more than once during the same execution', () => {
+        const obj = new JObject();
+        expect(obj.hashCode()).to.be.equal(obj.hashCode());
+    });
+
+    it('hashCode on two objects should return the same integer if the two objects are equal according to the equals method', () => {
+        const obj = new JObject();
+        const obj2 = new JObject();
+
+        if (obj.equals(obj2))
+            expect(obj.hashCode()).to.be.equal(obj2.hashCode());
+        else
+            expect(obj.hashCode()).to.be.not.equal(obj2.hashCode());
+    });
+
+    it('clone should result in throwing an exception because JObject does not itself implement the interface Cloneable', () => {
+        const obj = new FakeJObject();
+        expect(obj.clone).to.throw("CloneNotSupportedException");
+    });
+
+    it('toString should return a string representation of the object', () => {
+        const obj = new JObject();
+        const objhash = obj.hashCode();
+        expect(obj.toString()).to.be.equal(`java.lang.Object@${objhash.toString(16)}`);
+    });
+
+});
+
+class FakeJObject extends JObject {
+    clone(): JObject {
+        super.clone();
+        return new JObject();
+    }
+}
