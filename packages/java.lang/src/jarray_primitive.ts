ba6b5b1ab1472525e39b99bcc658cadccf1fdf1d
Francesco Dammacco <Fra@MacBook-Air-di-Francesco.local>
2017-07-26 00:15:16 +0200

    feat(Jarray): first implementation of Jarray primitive
    21

---
+++ packages/java.lang/src/jarray_primitive.ts
@@ -0,0 +1,75 @@
+import {JObject} from '../src/jobject';
+import {is} from '../src/jboolean_primitive';
+import {Jint, jint} from '../src/jint_primitive';
+
+/**
+ * An array is a container object that holds a fixed number of values of a single type.
+ * The length of an array is established when the array is created. After creation, its length is fixed.
+ * Each item in an array is called an element, and each element is accessed by its numerical index.
+ * Arrays are objects, are dynamically created, and may be assigned to variables of type JObject.
+ * All methods of class JObject may be invoked on an array.
+ */
+export class Jarray<T> extends JObject {
+  private _array: T[];
+  /**
+   * The size of the array.
+   */
+  public length: Jint;
+
+  /**
+   * Initialize an empty array of the specified size.
+   * @param {Jint} size the size of the array.
+   */
+  constructor(size: Jint); // TODO Passare default come parametro per inizializzazione? (Advanced)
+  /**
+   * Initialize an array with the specified values, inferring its size.
+   * @param {T[]} values the values with which initialize the array.
+   */
+  constructor(values: T[]);
+  constructor(arg: Jint | T[]) {
+    super();
+
+    if (Array.isArray(arg)) {
+      this._array = arg;
+      this.length = jint(arg.length);
+    } else {
+      this._array = new Array<T>(arg.unwrap());
+      this.length = arg;
+    }
+  }
+
+  /**
+   * Sets the specified value in the specified index of the array. The index bust be greater or equal to zero and less than the array size.
+   * If index exceedes these bounds a JArrayOutOfBoundsException is thrown.
+   * @param {Jint} index the position index of the array in which insert the new element.
+   * @param {T} value the value to insert.
+   */
+  public set(index: Jint, value: T) {
+    if (is(index.gt(jint(this.length.unwrap() - 1))) || is(index.lt(jint(0)))) {
+      // TODO JArrayOutOfBoundsException
+      throw Error(`JArrayOutOfBoundsException: ${index.unwrap()}`);
+    }
+
+    this._array[index.unwrap()] = value;
+  }
+
+  /**
+   * Returns the element of the array in the specified index. The index bust be greater or equal to zero and less than the array size.
+   * If index exceedes these bounds a JArrayOutOfBoundsException is thrown.
+   * @param {Jint} index the position index of the array from which retrieve the element.
+   * @returns {T} the array element retrieved in the specified position index.
+   */
+  public get(index: Jint): T {
+    if (is(index.gt(jint(this.length.unwrap() - 1))) || is(index.lt(jint(0)))) {
+      // TODO JArrayOutOfBoundsException
+      throw Error(`JArrayOutOfBoundsException: ${index.unwrap()}`);
+    }
+
+    return this._array[index.unwrap()]; // Se togliamo .unwrap(), come faccio gli accessi?
+  }
+
+  // TODO mock, remove when getClass() in JObject is implemented
+  public toString(): string {
+    return `[I@${this.hashCode().unwrap().toString(16)}`;
+  }
+}
---
+++ packages/java.lang/test/jarray_primitive.spec.ts
@@ -0,0 +1,100 @@
+/* tslint:disable:no-unused-expression */
+import {expect} from 'chai';
+import {Jint, jint} from '../src/jint_primitive';
+import {Jarray} from '../src/jarray_primitive';
+import {is} from '../src/jboolean_primitive';
+
+describe('Jarray', () => {
+  it('should construct an array of the specified size', () => {
+    const array = new Jarray<Jint>(jint(3));
+    const arraySize = jint(3);
+
+    expect(is(array.length.eq(arraySize))).to.be.true;
+  });
+
+  it('should construct and initialize an array with specified values, inferring its size', () => {
+    const index1 = jint(0);
+    const index2 = jint(1);
+    const index3 = jint(2);
+
+    const value1 = jint(1);
+    const value2 = jint(2);
+    const value3 = jint(3);
+
+    const values = [value1, value2, value3];
+    const valuesSize = jint(values.length);
+
+    const array = new Jarray<Jint>(values);
+
+    expect(is(array.get(index1).eq(value1))).to.be.true;
+    expect(is(array.get(index2).eq(value2))).to.be.true;
+    expect(is(array.get(index3).eq(value3))).to.be.true;
+    expect(is(array.length.eq(valuesSize))).to.be.true;
+  });
+
+  xit('should return default value for non initialized array', () => {
+    const array = new Jarray<Jint>(jint(3));
+    const index = jint(0);
+    const defaultValue = jint(0);
+
+    expect(is(array.get(index).eq(defaultValue))).to.be.true;
+  });
+
+  it('should set and get values in positions less than its size', () => {
+    const array = new Jarray<Jint>(jint(3));
+
+    const index1 = jint(0);
+    const index2 = jint(1);
+    const index3 = jint(2);
+
+    const value1 = jint(1);
+    const value2 = jint(2);
+    const value3 = jint(3);
+
+    array.set(index1, value1);
+    array.set(index2, value2);
+    array.set(index3, value3);
+
+    expect(is(array.get(index1).eq(value1))).to.be.true;
+    expect(is(array.get(index2).eq(value2))).to.be.true;
+    expect(is(array.get(index3).eq(value3))).to.be.true;
+  });
+
+  it('should result in throwing an exception when accessing positions beyond its size', () => {
+    const array = new Jarray<Jint>(jint(3));
+
+    const defaultValue = jint(0);
+
+    const invalidIndex1 = jint(-1);
+    const invalidIndex2 = jint(3);
+    const invalidIndex3 = jint(4);
+
+    expect(() => array.set(invalidIndex1, defaultValue)).to.throw(`JArrayOutOfBoundsException: ${invalidIndex1.unwrap()}`);
+    expect(() => array.set(invalidIndex2, defaultValue)).to.throw(`JArrayOutOfBoundsException: ${invalidIndex2.unwrap()}`);
+    expect(() => array.set(invalidIndex3, defaultValue)).to.throw(`JArrayOutOfBoundsException: ${invalidIndex3.unwrap()}`);
+
+    expect(() => array.get(invalidIndex1)).to.throw(`JArrayOutOfBoundsException: ${invalidIndex1.unwrap()}`);
+    expect(() => array.get(invalidIndex2)).to.throw(`JArrayOutOfBoundsException: ${invalidIndex2.unwrap()}`);
+    expect(() => array.get(invalidIndex3)).to.throw(`JArrayOutOfBoundsException: ${invalidIndex3.unwrap()}`);
+  });
+
+  // TODO remove when getClass() in JObject is implemented, already tested in JObject
+  it('toString should return a string representation of the object', () => {
+    const array = new Jarray<Jint>(jint(3));
+    const arrayhash = array.hashCode();
+
+    const index1 = jint(0);
+    const index2 = jint(1);
+    const index3 = jint(2);
+
+    const value1 = jint(1);
+    const value2 = jint(2);
+    const value3 = jint(3);
+
+    array.set(index1, value1);
+    array.set(index2, value2);
+    array.set(index3, value3);
+
+    expect(array.toString()).to.be.equal(`[I@${arrayhash.unwrap().toString(16)}`);
+  });
+});
