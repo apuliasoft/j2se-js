4b2ae5d0fce78a21a2e5f166142daa50cb235cec
Francesco Dammacco <Fra@MacBook-Air-di-Francesco.local>
2017-07-25 21:29:20 +0200

    feat(condition framework): implemented Jarithmetic and JUnary for Jint
    also added initialization by string (decimal, hexadecimal or binary) and a factory constructor
    
    23

--- packages/java.lang/src/jint_primitive.ts
+++ packages/java.lang/src/jint_primitive.ts
@@ -1,10 +1,11 @@
-import { JEquality } from '@j2se-js/java.lang.native.operator';
-import { JRelational } from '@j2se-js/java.lang.native.operator';
-import { Jboolean } from './jboolean_primitive';
-import { JObject } from './jobject';
+import {JEquality} from '@j2se-js/java.lang.native.operator';
+import {JRelational} from '@j2se-js/java.lang.native.operator';
+import {Jboolean} from './jboolean_primitive';
+import {JUnary} from '@j2se-js/java.lang.native.operator/src/junary';
+import {JArithmetic} from '@j2se-js/java.lang.native.operator/src/jarithmetic';
 
 /**
- * By default, the jint data type is a 32-bit signed two's complement integer,
+ * By default, the Jint data type is a 32-bit signed two's complement integer,
  * which has a minimum value of -2^31 and a maximum value of 2^31-1.
  * In Java SE 8 and later, you can use the jint data type to represent an unsigned
  * 32-bit integer, which has a minimum value of 0 and a maximum value of 2^32-1.
@@ -13,55 +14,142 @@ import { JObject } from './jobject';
  * Static methods like compareUnsigned, divideUnsigned etc have been added to
  * the Integer class to support the arithmetic operations for unsigned integers.
  *
- * Note: To retrieve the actual boolean value of a jint you have to use {@code .value} syntax.
+ * Note: To retrieve the actual numeric value of a Jint you have to use {@code .value} syntax.
  */
-export class Jint implements JEquality<Jint>, JRelational<Jint> {
-    private _value: Int32Array;
+export class Jint implements JEquality<Jint>, JRelational<Jint>, JUnary<Jint>, JArithmetic<Jint> {
+  /**
+   * Internal factory for constructing a Jint without use the new keyword.
+   * @param {number | string} value to be wrapped in the new Jint.
+   * @returns {Jint} the Jint created.
+   */
+  static create(value: number | string = 0): Jint {
+    return new Jint(value);
+  }
 
-    constructor(value: number = 0) {
-        this._value = new Int32Array(1);
-        this.value = value;
+  private static parse(value: string): number {
+    if (value.match('0b.*')) {
+      return parseInt(value.slice(2, value.length), 2);
+    } else if (value.match('0x.*')) {
+      return parseInt(value.slice(2, value.length), 16);
+    } else {
+      return parseInt(value, 10);
     }
+  }
 
-    get value() {
-        return this._value[0];
+  private _value: Int32Array;
+
+  private constructor(value: number | string = 0) {
+    this._value = new Int32Array(1);
+
+    if (typeof value === 'number') {
+      this.value = value;
+    } else if (typeof value === 'string') {
+      const isNegative = value.charAt(0) === '-';
+
+      if (isNegative) {
+        value = value.slice(1, value.length);
+        this.value = -Jint.parse(value);
+      } else {
+        this.value = Jint.parse(value);
+      }
     }
+  }
 
-    /**
-     * Set int value in respect of the Java int costraint.
-     * In order to simulate 32 bit signed int, is used a
-     * Int32Array with just 1 element.
-     */
-    set value(value: number) {
-        this._value[0] = value;
-    }
+  private get value(): number {
+    return this._value[0];
+  }
 
-    public eq(expr: Jint): Jboolean {
-        return new Jboolean(this.value === expr.value);
-    }
+  /**
+   * Set int value in respect of the Java int costraint.
+   * In order to simulate 32 bit signed int, is used a
+   * Int32Array with just 1 element.
+   */
+  private set value(value: number) {
+    this._value[0] = value;
+  }
 
-    public ne(expr: Jint): Jboolean {
-        return new Jboolean(this.value !== expr.value);
-    }
+  /**
+   * Retrieve the numeric value wrapped by this Jint.
+   * @returns {number} numeric value wrapped by this Jint.
+   */
+  public unwrap(): number {
+    return this.value;
+  }
 
-    public lt(expr: Jint): Jboolean {
-        return new Jboolean(this.value < expr.value);
-    }
+  public eq(expr: Jint): Jboolean {
+    return new Jboolean(this.value === expr.value);
+  }
 
-    public gt(expr: Jint): Jboolean {
-        return new Jboolean(this.value > expr.value);
-    }
+  public ne(expr: Jint): Jboolean {
+    return new Jboolean(this.value !== expr.value);
+  }
 
-    public le(expr: Jint): Jboolean {
-        return new Jboolean(this.value <= expr.value);
-    }
+  public lt(expr: Jint): Jboolean {
+    return new Jboolean(this.value < expr.value);
+  }
 
-    public ge(expr: Jint): Jboolean {
-        return new Jboolean(this.value >= expr.value);
-    }
+  public gt(expr: Jint): Jboolean {
+    return new Jboolean(this.value > expr.value);
+  }
 
-    public instanceof(t: Function): Jboolean {
-        throw new Error('Method instanceof not implemented.');
-    }
+  public le(expr: Jint): Jboolean {
+    return new Jboolean(this.value <= expr.value);
+  }
 
+  public ge(expr: Jint): Jboolean {
+    return new Jboolean(this.value >= expr.value);
+  }
+
+  public instanceof(t: Function): Jboolean {
+    throw new Error('Method instanceof not implemented.');
+  }
+
+  public plus(): Jint {
+    return jint(+(this.value));
+  }
+
+  public inc(): Jint {
+    return jint(this.value + 1);
+  }
+
+  public dec(): Jint {
+    return jint(this.value - 1);
+  }
+
+  public minus(): Jint {
+    return jint(-(this.value));
+  }
+
+  add(expr: Jint): Jint {
+    return jint(this.value + expr.value);
+  }
+
+  sub(expr: Jint): Jint {
+    return jint(this.value - expr.value);
+  }
+
+  mul(expr: Jint): Jint {
+    return jint(this.value * expr.value);
+  }
+
+  div(expr: Jint): Jint {
+    return jint(this.value / expr.value);
+  }
+
+  mod(expr: Jint): Jint {
+    return jint(this.value % expr.value);
+  }
+
+  public toString(): string {
+    return this._value.toString();
+  }
+}
+
+/**
+ * Factory for constructing a Jint without use the new keyword. It calls {@link Jint#create} method.
+ * @param {number | string} value to be wrapped in the new Jint.
+ * @returns {Jint} the Jint created.
+ */
+export function jint(value: number | string = 0): Jint {
+  return Jint.create(value);
 }
--- packages/java.lang/test/jint_primitive.spec.ts
+++ packages/java.lang/test/jint_primitive.spec.ts
@@ -1,65 +1,153 @@
 /* tslint:disable:no-unused-expression */
-import { expect } from 'chai';
-import { Jint } from '../src/jint_primitive';
-import { is } from '../src/jboolean_primitive';
+import {expect} from 'chai';
+import {Jint, jint} from '../src/jint_primitive';
+import {is} from '../src/jboolean_primitive';
 
 describe('Jint', () => {
 
-    it('should have 0 as default value.', () => {
-        const i = new Jint();
+  it('should have 0 as default value.', () => {
+    const i = jint();
 
-        expect(i.value).to.be.eq(0);
-    });
+    expect(i.unwrap()).to.be.eq(0);
+  });
 
-    it('should be 1 when set to 1.9', () => {
-        const i = new Jint(1.9);
+  it('should be 1 when set to 1.9', () => {
+    const i = jint(1.9);
+    const j = jint('1.9');
+    const im = jint(-1.9);
+    const jm = jint('-1.9');
 
-        expect(i.value).to.be.eq(1);
-    });
+    expect(i.unwrap()).to.be.eq(1);
+    expect(j.unwrap()).to.be.eq(1);
+    expect(im.unwrap()).to.be.eq(-1);
+    expect(jm.unwrap()).to.be.eq(-1);
+  });
 
-    it('should overflow when a number grater then 2^31-1 is set.', () => {
-        const i = new Jint(Math.pow(2, 31));
+  it('should be initializable with hexadecimal', () => {
+    const i = jint('0x1a');
+    const j = jint('-0x1a');
 
-        expect(i.value).to.be.eq(-Math.pow(2, 31));
-    });
+    expect(i.unwrap()).to.be.eq(26);
+    expect(j.unwrap()).to.be.eq(-26);
+  });
 
-    it('should overflow when a number lower then -2^31 is set.', () => {
-        const i = new Jint(-Math.pow(2, 31) - 1);
+  it('should be initializable with binary', () => {
+    const i = jint('0b11010');
+    const j = jint('-0b11010');
 
-        expect(i.value).to.be.eq(Math.pow(2, 31) - 1);
-    });
+    expect(i.unwrap()).to.be.eq(26);
+    expect(j.unwrap()).to.be.eq(-26);
+  });
 
-    it('new Jint(24) should be equal to new Jint(24)', () => {
-        const i1 = new Jint(24);
-        const i2 = new Jint();
-        i2.value = 24;
 
-        expect(is(i1.eq(i2))).to.be.true;
-        expect(is(i1.ne(i2))).to.be.false;
-    });
+  it('should overflow when a number grater then 2^31-1 is set.', () => {
+    const i = jint(Math.pow(2, 31));
 
-    it('new Jint(5) should be not equal to new Jint(6)', () => {
-        const i1 = new Jint(5);
-        const i2 = new Jint();
-        i2.value = 6;
+    expect(i.unwrap()).to.be.eq(-Math.pow(2, 31));
+  });
 
-        expect(is(i1.eq(i2))).to.be.false;
-        expect(is(i1.ne(i2))).to.be.true;
-    });
+  it('should overflow when a number lower then -2^31 is set.', () => {
+    const i = jint(-Math.pow(2, 31) - 1);
 
-    it('new Jint(5) should be lower then new Jint(6)', () => {
-        const i1 = new Jint(5);
-        const i2 = new Jint(6);
+    expect(i.unwrap()).to.be.eq(Math.pow(2, 31) - 1);
+  });
 
-        expect(is(i1.lt(i2))).to.be.true;
-        expect(is(i1.ge(i2))).to.be.false;
-    });
+  it('jint(24) should be equal to jint(24)', () => {
+    const i1 = jint(24);
+    const i2 = jint(24);
 
-    it('new Jint(6) should be greater than new Jint(5)', () => {
-        const i1 = new Jint(6);
-        const i2 = new Jint(5);
+    expect(is(i1.eq(i2))).to.be.true;
+    expect(is(i1.ne(i2))).to.be.false;
+  });
 
-        expect(is(i1.gt(i2))).to.be.true;
-        expect(is(i1.le(i2))).to.be.false;
-    });
+  it('jint(5) should be not equal to jint(6)', () => {
+    const i1 = jint(5);
+    const i2 = jint(6);
+
+    expect(is(i1.eq(i2))).to.be.false;
+    expect(is(i1.ne(i2))).to.be.true;
+  });
+
+  it('jint(5) should be lower then jint(6)', () => {
+    const i1 = jint(5);
+    const i2 = jint(6);
+
+    expect(is(i1.lt(i2))).to.be.true;
+    expect(is(i1.ge(i2))).to.be.false;
+  });
+
+  it('jint(6) should be greater than jint(5)', () => {
+    const i1 = jint(6);
+    const i2 = jint(5);
+
+    expect(is(i1.gt(i2))).to.be.true;
+    expect(is(i1.le(i2))).to.be.false;
+  });
+
+  it('+(jint(-1)) should be equal to jint(-1)', () => {
+    const i = jint(-1);
+
+    expect(is(i.plus().eq(i))).to.be.true;
+  });
+
+  it('-(jint(-1)) should be equal to jint(1)', () => {
+    const i = jint(-1);
+    const j = jint(1);
+
+    expect(is(i.minus().eq(j))).to.be.true;
+  });
+
+  it('++(jint(-1)) should be equal to jint(0)', () => {
+    const i = jint(-1);
+    const j = jint(0);
+
+    expect(is(i.inc().eq(j))).to.be.true;
+  });
+
+  it('--(jint(-1)) should be equal to jint(-2)', () => {
+    const i = jint(-1);
+    const j = jint(-2);
+
+    expect(is(i.dec().eq(j))).to.be.true;
+  });
+
+  it('jint(6) + jint(5) should be equal to jint(11)', () => {
+    const i1 = jint(6);
+    const i2 = jint(5);
+    const res = jint(11);
+
+    expect(is(i1.add(i2).eq(res))).to.be.true;
+  });
+
+  it('jint(6) - jint(5) should be equal to jint(1)', () => {
+    const i1 = jint(6);
+    const i2 = jint(5);
+    const res = jint(1);
+
+    expect(is(i1.sub(i2).eq(res))).to.be.true;
+  });
+
+  it('jint(6) * jint(5) should be equal to jint(30)', () => {
+    const i1 = jint(6);
+    const i2 = jint(5);
+    const res = jint(30);
+
+    expect(is(i1.mul(i2).eq(res))).to.be.true;
+  });
+
+  it('jint(12) / jint(6) should be equal to jint(2)', () => {
+    const i1 = jint(12);
+    const i2 = jint(6);
+    const res = jint(2);
+
+    expect(is(i1.div(i2).eq(res))).to.be.true;
+  });
+
+  it('jint(12) % jint(6) should be equal to jint(0)', () => {
+    const i1 = jint(12);
+    const i2 = jint(6);
+    const res = jint(0);
+
+    expect(is(i1.mod(i2).eq(res))).to.be.true;
+  });
 });
