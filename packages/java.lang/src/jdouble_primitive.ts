df001a3a5fe5450289e3a773019d8c90caa450f6
Francesco Dammacco <Fra@MacBook-Air-di-Francesco.local>
2017-07-27 12:45:54 +0200

    feat(condition framework): implemented JArithmetic ad JUnary for Jdouble
    also added initialization by string and factory constructor
    
    24

--- packages/java.lang/src/jdouble_primitive.ts
+++ packages/java.lang/src/jdouble_primitive.ts
@@ -1,6 +1,16 @@
 import {JEquality} from '../../java.lang.native.operator/src/jequality';
 import {Jboolean, jboolean} from '../../java.lang/src/jboolean_primitive';
 import {JRelational} from '../../java.lang.native.operator/src/jrelational';
+import {JUnary} from '@j2se-js/java.lang.native.operator/src/junary';
+import {JArithmetic} from '@j2se-js/java.lang.native.operator/src/jarithmetic';
+
+const longRegex = /^\d+l$/i;
+const doubleRegex = /^(?:(?:\d*\.\d+)|(?:\d+\.\d*))(?:e[+-]?\d+)?d?$|^-?\d+d$/i;
+const floatRegex = /^(?:(?:\d*\.?\d+)|(?:\d+\.?\d*))(?:e[+-]?\d+)?f$/i;
+
+const binaryRegex = /^0b[01]+$/i;
+const hexRegex = /^0x[0123456789abcdef]+$/i;
+const intRegex = /^\d+$/;
 
 /**
  * The double data type is a double-precision 64-bit IEEE 754 floating point.
@@ -10,21 +20,46 @@ import {JRelational} from '../../java.lang.native.operator/src/jrelational';
  * choice. As mentioned above, this data type should never be used for precise
  * values, such as currency.
  *
- * Note: To retrieve the actual numeric _value of a Jdouble you have to use {@code ._value} syntax.
+ * Note: To retrieve the actual numeric value wrapped in a Jdouble you have to use <code>.value</code> syntax.
  */
-export class Jdouble implements JEquality<Jdouble>, JRelational<Jdouble> {
+export class Jdouble implements JEquality<Jdouble>, JRelational<Jdouble>, JUnary<Jdouble>, JArithmetic<Jdouble> {
+  /**
+   * Internal factory for constructing a Jdouble without use the new keyword.
+   * @param {number | string} value to be wrapped in the new Jdouble.
+   * @returns {Jdouble} the Jdouble created.
+   */
+  static create(value: number | string = 0): Jdouble {
+    return new Jdouble(value);
+  }
+
+  private static validate(value: string) {
+    if (!value.match(longRegex) &&
+      !value.match(doubleRegex) &&
+      !value.match(floatRegex) &&
+      !value.match(hexRegex) &&
+      !value.match(binaryRegex) &&
+      !value.match(intRegex)) {
+
+      throw Error('incompatible types: string cannot be converted to double');
+    }
+  }
+
   private _value: number;
 
-  constructor(value: number = 0) {
-    this.value = value;
+  private constructor(value: number | string = 0) {
+    let isNegative = false;
+    if (typeof value === 'string') {
+      value = value.replace(/^\+/, '');
+      isNegative = value.charAt(0) === '-';
+      Jdouble.validate(isNegative ? value = value.slice(1, value.length) : value);
+      value = value.replace(/[dfl]$/i, '');
+    }
+
+    this._value = isNegative ? -value : +value;
   }
 
-  get value() {
+  public get value() {
     return this._value;
-  }
-
-  set value(value: number) {
-    this._value = value;
   }
 
   public eq(expr: Jdouble): Jboolean {
@@ -50,4 +85,53 @@ export class Jdouble implements JEquality<Jdouble>, JRelational<Jdouble> {
   public ge(expr: Jdouble): Jboolean {
     return jboolean(this.value >= expr.value);
   }
+
+  public plus(): Jdouble {
+    return jdouble(+(this._value));
+  }
+
+  public inc(): Jdouble {
+    return jdouble(this._value + 1);
+  }
+
+  public dec(): Jdouble {
+    return jdouble(this._value - 1);
+  }
+
+  public minus(): Jdouble {
+    return jdouble(-(this._value));
+  }
+
+  public add(expr: Jdouble): Jdouble {
+    return jdouble(this._value + expr._value);
+  }
+
+  public sub(expr: Jdouble): Jdouble {
+    return jdouble(this._value - expr._value);
+  }
+
+  public mul(expr: Jdouble): Jdouble {
+    return jdouble(this._value * expr._value);
+  }
+
+  public div(expr: Jdouble): Jdouble {
+    return jdouble(this._value / expr._value);
+  }
+
+  public mod(expr: Jdouble): Jdouble {
+    return jdouble(this._value % expr._value);
+  }
+
+  public toString(): string {
+    return this._value.toString();
+  }
+}
+
+/**
+ * Factory for constructing a Jdouble without use the new keyword. It calls {@link Jdouble#create} method.
+ * @param {number | string} value to be wrapped in the new Jdouble.
+ * @returns {Jdouble} the Jdouble created.
+ */
+export function jdouble(value: number | string = 0): Jdouble {
+  return Jdouble.create(value);
 }
--- packages/java.lang/test/jdouble_primitive.spec.ts
+++ packages/java.lang/test/jdouble_primitive.spec.ts
@@ -1,66 +1,223 @@
 /* tslint:disable:no-unused-expression */
-import { expect } from 'chai';
-import { Jdouble } from '../src/jdouble_primitive';
-import { is } from '../src/jboolean_primitive';
+import {expect} from 'chai';
+import {Jdouble, jdouble} from '../src/jdouble_primitive';
+import {is} from '../src/jboolean_primitive';
 
 describe('Jdouble', () => {
 
-    it('should have 0 as default _value', () => {
-        const i = new Jdouble();
+  it('should have 0 as default value', () => {
+    const i = jdouble();
 
-        expect(i.value).to.be.eq(0);
-    });
+    expect(i.value).to.be.eq(0);
+  });
 
-    it('should be 1 when set to 1.9', () => {
-        const i = new Jdouble(1.9);
+  it('should be initializable with number (positive or negative)', () => {
+    const i = jdouble(+2.6);
+    const j = jdouble(-2.);
+    const i2 = jdouble('+2');
+    const j2 = jdouble('-2.6');
 
-        expect(i.value).to.be.eq(1.9);
-    });
+    expect(i.value).to.be.eq(2.6);
+    expect(j.value).to.be.eq(-2.);
+    expect(i2.value).to.be.eq(2);
+    expect(j2.value).to.be.eq(-2.6);
+  });
 
-    it('should be max 1.7976931348623157E308 without overflow', () => {
-        const max = 1.7976931348623157E308;
-        const i = new Jdouble(max + 1);
+  it('should be initializable with hexadecimal (positive or negative)', () => {
+    const i = jdouble(0x1a);
+    const j = jdouble(-0x1a);
+    const i2 = jdouble('+0X1a');
+    const j2 = jdouble('-0x1a');
 
-        expect(i.value).to.be.eq(max);
-    });
+    expect(i.value).to.be.eq(26);
+    expect(j.value).to.be.eq(-26);
+    expect(i2.value).to.be.eq(26);
+    expect(j2.value).to.be.eq(-26);
+  });
 
-    it('should be min 4.9E-324 and the MIN+1 should be 1', () => {
-        const i = new Jdouble((4.9E-324) + 1);
+  it('should be initializable with binary (positive or negative)', () => {
+    const i = jdouble(+0b11010);
+    const j = jdouble(-0b11010);
+    const i2 = jdouble('0B11010');
+    const j2 = jdouble('-0b11010');
 
-        expect(i.value).to.be.eq(1);
-    });
+    expect(i.value).to.be.eq(26);
+    expect(j.value).to.be.eq(-26);
+    expect(i2.value).to.be.eq(26);
+    expect(j2.value).to.be.eq(-26);
+  });
 
-    it('new Jdouble(24) should be equal to new Jdouble(24)', () => {
-        const i1 = new Jdouble(24);
-        const i2 = new Jdouble();
-        i2.value = 24;
+  it('should be initializable with long (positive or negative)', () => {
+    const i = jdouble('26l');
+    const j = jdouble('-26l');
+    expect(i.value).to.be.eq(26);
+    expect(j.value).to.be.eq(-26);
+  });
 
-        expect(is(i1.eq(i2))).to.be.true;
-        expect(is(i1.ne(i2))).to.be.false;
-    });
+  it('should be initializable with float (positive or negative)', () => {
+    const d = jdouble('26F');
+    const d2 = jdouble('-26F');
+    const d3 = jdouble('2.6F');
+    const d4 = jdouble('-2.6e1f');
+    const d5 = jdouble('.6F');
+    const d6 = jdouble('-.6F');
+    const d7 = jdouble('2.F');
+    const d8 = jdouble('-2.F');
 
-    it('new Jdouble(5) should be not equal to new Jdouble(6)', () => {
-        const i1 = new Jdouble(5);
-        const i2 = new Jdouble();
-        i2.value = 6;
+    expect(d.value).to.be.eq(26.0);
+    expect(d2.value).to.be.eq(-26.0);
+    expect(d3.value).to.be.eq(2.6);
+    expect(d4.value).to.be.eq(-26);
+    expect(d5.value).to.be.eq(0.6);
+    expect(d6.value).to.be.eq(-0.6);
+    expect(d7.value).to.be.eq(2.0);
+    expect(d8.value).to.be.eq(-2.0);
+  });
 
-        expect(is(i1.eq(i2))).to.be.false;
-        expect(is(i1.ne(i2))).to.be.true;
-    });
+  it('should be initializable with double (positive or negative)', () => {
+    const d = jdouble('26d');
+    const d2 = jdouble('-26D');
+    const d3 = jdouble('2.6D');
+    const d4 = jdouble('-2.6D');
+    const d5 = jdouble('.6d');
+    const d6 = jdouble('-.6e1d');
+    const d7 = jdouble('2.d');
+    const d8 = jdouble('-2.D');
 
-    it('new Jdouble(5) should be lower then new Jdouble(6)', () => {
-        const i1 = new Jdouble(5);
-        const i2 = new Jdouble(6);
+    expect(d.value).to.be.eq(26.0);
+    expect(d2.value).to.be.eq(-26.0);
+    expect(d3.value).to.be.eq(2.6);
+    expect(d4.value).to.be.eq(-2.6);
+    expect(d5.value).to.be.eq(0.6);
+    expect(d6.value).to.be.eq(-6);
+    expect(d7.value).to.be.eq(2.0);
+    expect(d8.value).to.be.eq(-2.0);
+  });
 
-        expect(is(i1.lt(i2))).to.be.true;
-        expect(is(i1.ge(i2))).to.be.false;
-    });
+  it('should not be initializable with an invalid string', () => {
+    const errorMessage = 'incompatible types: string cannot be converted to double';
+    expect(() => jdouble('hello world')).to.throw(errorMessage);
+    expect(() => jdouble('3.0.2')).to.throw(errorMessage);
+    expect(() => jdouble('.f')).to.throw(errorMessage);
+    expect(() => jdouble('a26')).to.throw(errorMessage);
+  });
 
-    it('new Jdouble(6) should be greater than new Jdouble(5)', () => {
-        const i1 = new Jdouble(6);
-        const i2 = new Jdouble(5);
+  it('should be max 1.7976931348623157E308 without overflow', () => {
+    const max = 1.7976931348623157E308;
+    const i = jdouble(max + 1);
 
-        expect(is(i1.gt(i2))).to.be.true;
-        expect(is(i1.le(i2))).to.be.false;
-    });
+    expect(i.value).to.be.eq(max);
+  });
+
+  it('should be min 4.9E-324 and the MIN+1 should be 1', () => {
+    const i = jdouble((4.9E-324) + 1);
+
+    expect(i.value).to.be.eq(1);
+  });
+
+  it('jdouble(24) should be equal to jdouble(24)', () => {
+    const i1 = jdouble(24);
+    const i2 = jdouble(24);
+
+    expect(is(i1.eq(i2))).to.be.true;
+    expect(is(i1.ne(i2))).to.be.false;
+  });
+
+  it('jdouble(5) should be not equal to jdouble(6)', () => {
+    const i1 = jdouble(5);
+    const i2 = jdouble(6);
+
+    expect(is(i1.eq(i2))).to.be.false;
+    expect(is(i1.ne(i2))).to.be.true;
+  });
+
+  it('jdouble(5) should be lower then jdouble(6)', () => {
+    const i1 = jdouble(5);
+    const i2 = jdouble(6);
+
+    expect(is(i1.lt(i2))).to.be.true;
+    expect(is(i1.ge(i2))).to.be.false;
+  });
+
+  it('jdouble(6) should be greater than jdouble(5)', () => {
+    const i1 = jdouble(6);
+    const i2 = jdouble(5);
+
+    expect(is(i1.gt(i2))).to.be.true;
+    expect(is(i1.le(i2))).to.be.false;
+  });
+
+
+  it('+(jdouble(-1)) should be equal to jdouble(-1)', () => {
+    const i = jdouble(-1);
+
+    expect(is(i.plus().eq(i))).to.be.true;
+  });
+
+  it('-(jdouble(-1)) should be equal to jdouble(1)', () => {
+    const i = jdouble(-1);
+    const j = jdouble(1);
+
+    expect(is(i.minus().eq(j))).to.be.true;
+  });
+
+  it('++(jdouble(-1)) should be equal to jdouble(0)', () => {
+    const i = jdouble(-1);
+    const j = jdouble(0);
+
+    expect(is(i.inc().eq(j))).to.be.true;
+  });
+
+  it('--(jdouble(-1)) should be equal to jdouble(-2)', () => {
+    const i = jdouble(-1);
+    const j = jdouble(-2);
+
+    expect(is(i.dec().eq(j))).to.be.true;
+  });
+
+  it('jdouble(6) + jdouble(5) should be equal to jdouble(11)', () => {
+    const i1 = jdouble(6);
+    const i2 = jdouble(5);
+    const res = jdouble(11);
+
+    expect(is(i1.add(i2).eq(res))).to.be.true;
+  });
+
+  it('jdouble(6) - jdouble(5) should be equal to jdouble(1)', () => {
+    const i1 = jdouble(6);
+    const i2 = jdouble(5);
+    const res = jdouble(1);
+
+    expect(is(i1.sub(i2).eq(res))).to.be.true;
+  });
+
+  it('jdouble(6) * jdouble(5) should be equal to jdouble(30)', () => {
+    const i1 = jdouble(6);
+    const i2 = jdouble(5);
+    const res = jdouble(30);
+
+    expect(is(i1.mul(i2).eq(res))).to.be.true;
+  });
+
+  it('jdouble(12) / jdouble(6) should be equal to jdouble(2)', () => {
+    const i1 = jdouble(12);
+    const i2 = jdouble(6);
+    const res = jdouble(2);
+
+    expect(is(i1.div(i2).eq(res))).to.be.true;
+  });
+
+  it('jdouble(12) % jdouble(6) should be equal to jdouble(0)', () => {
+    const i1 = jdouble(12);
+    const i2 = jdouble(6);
+    const res = jdouble(0);
+
+    expect(is(i1.mod(i2).eq(res))).to.be.true;
+  });
+
+  it('toString should convert a Jint in a string for printing', () => {
+    const i = jdouble(5.6);
+
+    expect(i.toString()).to.be.eq('5.6');
+  });
 });
