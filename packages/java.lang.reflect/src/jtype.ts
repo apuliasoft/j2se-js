bdeadc157f1f06bc617057da9b61133f1d458fbe
Francesco Dammacco <Fra@MacBook-Air-di-Francesco.local>
2017-07-28 19:57:00 +0200

    feat(java.lang.reflect): first implementation of JType interface

---
+++ packages/java.lang.reflect/src/jtype.ts
@@ -0,0 +1,19 @@
+/**
+ * Type is the common superinterface for all types in the Java
+ * programming language. These include raw types, parameterized types,
+ * array types, type variables and primitive types.
+ *
+ * @since 1.5
+ */
+export interface Type {
+  /**
+   * Returns a string describing this type, including information
+   * about any type parameters.
+   *
+   * @implSpec The default implementation calls {@code toString}.
+   *
+   * @return a string describing this type
+   * @since 1.8
+   */
+  getTypeName(): string; // TODO must return JString | see default implementation?
+}
