54e5efe620d7df18813ac65770a2395add6a1d97
Francesco Dammacco <Fra@MacBook-Air-di-Francesco.local>
2017-07-28 19:29:37 +0200

    feat(java.lang.reflect): first implementation of JAnnotatedElement and JGenericDeclaration interface

---
+++ packages/java.lang.reflect/src/jgenericdeclaration.ts
@@ -0,0 +1,24 @@
+import {JAnnotatedElement} from './jannotatedelement';
+
+/**
+ * A common interface for all entities that declare type variables.
+ *
+ * @since 1.5
+ */
+export interface JGenericDeclaration extends JAnnotatedElement {
+  /**
+   * Returns an array of {@code TypeVariable} objects that
+   * represent the type variables declared by the generic
+   * declaration represented by this {@code GenericDeclaration}
+   * object, in declaration order.  Returns an array of length 0 if
+   * the underlying generic declaration declares no type variables.
+   *
+   * @return an array of {@code TypeVariable} objects that represent
+   *     the type variables declared by this generic declaration
+   * @throws GenericSignatureFormatError if the generic
+   *     signature of this generic declaration does not conform to
+   *     the format specified in
+   *     <cite>The Java&trade; Virtual Machine Specification</cite>
+   */
+  // getTypeParameters(): Jarray<JTypeVariable<?>>
+}
